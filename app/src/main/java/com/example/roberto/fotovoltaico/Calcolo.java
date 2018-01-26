c9133c4456cb9be03265d1c88e64f3ca327930e7
Roberto Verrelli <yashi4891@gmail.com>
2018-01-26 16:08:58 +0100

    Finished battery dimensioning part1

--- app/src/main/java/com/example/roberto/fotovoltaico/Calcolo.java
+++ app/src/main/java/com/example/roberto/fotovoltaico/Calcolo.java
@@ -17,15 +17,19 @@ public class Calcolo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setto il layout xml che voglio usare(definito in activity_calcolo)
         setContentView(R.layout.activity_calcolo);
+        final String watt_received_from_main = getIntent().getExtras().getString("watt");
+        final int fabbisogno_giornaliero = Integer.valueOf(watt_received_from_main)*24;
         //setto il titolo dell'activity,l'id è titolo_activity cui definisco il suo layout in activity_calcolo.xml
         TextView titolo_activity = (TextView)findViewById(R.id.titolo_activity);
-        titolo_activity.setText("Dimensionamento batteria");
+        titolo_activity.setText("Giorni di autonomia senza sole?");
         //setto la textvie dove andro a scrivere il dato di watt inserito nella editext della mainactivity
         TextView ricevo_watt = (TextView)findViewById(R.id.ricevo_watt);
         //nella stringa watt_received_from_main inserisco il valore che ho inserito nella editext della mainactivity
-        final String watt_received_from_main = getIntent().getExtras().getString("watt");
+
         //setto la textview di cui sopra
         ricevo_watt.setText("Il tuo carico è di "+watt_received_from_main+" watt");
+        TextView fabbisogno_giornaliero_watt = (TextView)findViewById(R.id.fabbisogno_giornaliero_watt);
+        fabbisogno_giornaliero_watt.setText("Il fabbisogno giornaliero del carico è di  "+fabbisogno_giornaliero+" watt");
         //creo un'altra editext, dove andro ad inserire il valore dei giorni il cui carico deve rimanere acceso
         final EditText inserisco_giorni = (EditText) findViewById(R.id.inserisco_giorni);
         //setto il bottono eer passare alla successiva activity e ci passo i dati che ho messo nella editext di cui sopra
--- app/src/main/java/com/example/roberto/fotovoltaico/Calcolo2.java
+++ app/src/main/java/com/example/roberto/fotovoltaico/Calcolo2.java
@@ -16,23 +16,26 @@ public class Calcolo2 extends AppCompatActivity {
         //Intent intent = this.getIntent();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calcolo2);
+        final String giorni_received_from_calcolo = getIntent().getExtras().getString("giorni");
+        final String watt_received_from_main = getIntent().getExtras().getString("watt");
+        final int fabbisogno_giornaliero = Integer.valueOf(watt_received_from_main)*24;
+        final int valore_batteria = (fabbisogno_giornaliero*Integer.valueOf(giorni_received_from_calcolo))/12;
+
         //setto il titolo dell'activity,l'id è titolo_activity cui definisco il suo layout in activity_calcolo2.xml
         TextView titolo_activity = (TextView)findViewById(R.id.titolo_activity);
         titolo_activity.setText("Dimensionamento batteria");
         //setto la textview dove andro a scrivere il dato di watt inserito nella editext della mainactivity e da calcolo
         TextView ricevo_watt = (TextView)findViewById(R.id.ricevo_watt);
-        ricevo_watt.setText("Il tuo carico è di "+getIntent().getExtras().getString("watt")+" watt");
+        ricevo_watt.setText("Il tuo carico è di "+watt_received_from_main+" watt");
+        //setto la textview dove andro a scrivere il dato di fabbisogno giornaliero che calcolo come 24h * carico_watt
+        TextView fabbisogno_giornaliero_watt = (TextView)findViewById(R.id.fabbisogno_giornaliero);
+        fabbisogno_giornaliero_watt.setText("Il fabbisogno giornaliero del carico è di  "+fabbisogno_giornaliero+" watt");
         //setto la textview dove andro a scrivere il dato di giorni inserito nella editext della activity calcolo
         TextView ricevo_giorni = (TextView)findViewById(R.id.ricevo_giorni);
-        ricevo_giorni.setText("Il tuo ore è di "+getIntent().getExtras().getString("giorni")+" giorni");
+        ricevo_giorni.setText("Il numero di giorni senza sole è di "+giorni_received_from_calcolo+" giorni");
+        TextView valore_batteria_mah = (TextView)findViewById(R.id.valore_batteria_mah);
+        valore_batteria_mah.setText("La tua batterià dovrà essere di almeno "+valore_batteria+" mah");
 
-        final String giorni_received_from_calcolo = getIntent().getExtras().getString("giorni");
-        final String watt_received_from_main = getIntent().getExtras().getString("watt");
-
-        int risultato = Integer.valueOf(giorni_received_from_calcolo) * Integer.valueOf(watt_received_from_main);
-
-        TextView totale = (TextView)findViewById(R.id.totale);
-        totale.setText("Il tuo ore è di "+risultato+" mah");
 
         /*final EditText etLocation = (EditText) findViewById(R.id.editText);
 
